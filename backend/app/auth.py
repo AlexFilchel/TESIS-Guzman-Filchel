c2c04e2ad7d72d8ea22655816f86c012872dba62
Gianfranco Guzman <gianfranco.guzman.97@gmail.com>
2026-04-13 10:47:05 -0300

    fix: fixed remaining import paths

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -5,9 +5,9 @@ from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from config import settings
-from database import get_db
-from models import Usuario
+from app.config import settings
+from app.database import get_db
+from app.models import Usuario
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/login")
